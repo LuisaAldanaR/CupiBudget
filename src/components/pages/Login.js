f6e09a301c5b027ed3938736f8d36e6edeac146b
sightFelipe <juanfe092@gmail.com>
2023-09-19 18:19:09 -0500

    documentación correspondiente al login y css agregada

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import '../../../src/App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
-import md5 from 'md5';
-import Cookies from 'universal-cookie';
+import '../../../src/App.scss'; // Import styles from the specified path
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import axios from 'axios'; // Import the axios library for making HTTP requests
+import md5 from 'md5'; // Import the md5 library for hashing passwords
+import Cookies from 'universal-cookie'; // Import the universal-cookies library for managing cookies
 
-const baseUrl = "http://www.mendezmrf10.somee.com/api/Auth/login";
-const cookies = new Cookies();
+const baseUrl = "http://www.mendezmrf10.somee.com/api/Auth/login"; // Define the base URL for the API
+const cookies = new Cookies(); // Create a new instance of the Cookies class
 
 class Login extends Component {
   state = {
@@ -16,6 +16,7 @@ class Login extends Component {
     }
   }
 
+  // Function to handle input field changes
   handleChange = async (e) => {
     await this.setState({
       form: {
@@ -25,6 +26,7 @@ class Login extends Component {
     });
   }
 
+  // Function to initiate the login process
   iniciarSesion = async () => {
     await axios
       .get(baseUrl, { params: { username: this.state.form.username, password: md5(this.state.form.password) } })
@@ -34,15 +36,16 @@ class Login extends Component {
       .then((response) => {
         if (response.length > 0) {
           var respuesta = response[0];
+          // Set user information as cookies
           cookies.set('id', respuesta.id, { path: '/' });
           cookies.set('apellido_paterno', respuesta.apellido_paterno, { path: '/' });
           cookies.set('apellido_materno', respuesta.apellido_materno, { path: '/' });
           cookies.set('nombre', respuesta.nombre, { path: '/' });
           cookies.set('username', respuesta.username, { path: '/' });
-          alert(`Bienvenido ${respuesta.nombre} ${respuesta.apellido_paterno}`);
-          window.location.href = './menu';
+          alert(`Welcome ${respuesta.nombre} ${respuesta.apellido_paterno}`);
+          window.location.href = './menu'; // Redirect to the menu page on successful login
         } else {
-          alert('El usuario o la contraseña no son correctos');
+          alert('The username or password is incorrect'); // Show an error message if login fails
         }
       })
       .catch((error) => {
@@ -50,23 +53,24 @@ class Login extends Component {
       });
   }
 
+  // Function to check if a user is already logged in
   componentDidMount() {
     if (cookies.get('username')) {
-      window.location.href = './menu';
+      window.location.href = './menu'; // Redirect to the menu page if the user is already logged in
     }
   }
 
   render() {
     return (
       <div className="mainContainer">
-        <div className="welcomeContainer"> {/* Nuevo contenedor para el logo y el mensaje de bienvenida */}
-          <img src="ruta_al_logo.png" alt="Logo" className="logo" /> {/* Reemplaza "ruta_al_logo.png" por la ruta de tu logo */}
+        <div className="welcomeContainer"> {/* New container for the logo and welcome message */}
+          <img src="path_to_logo.png" alt="Logo" className="logo" /> {/* Replace "path_to_logo.png" with the path to your logo */}
           <p className="welcomeText">Bienvenido</p>
         </div>
         <div className="secondContainer">
           <br />
           <br />
-          <label className='l1'>Iniciar sesión: </label>
+          <label className='l1'>Iniciar Sesión </label>
           <br />
           <br />
           <br />
@@ -76,12 +80,12 @@ class Login extends Component {
               type="text"
               name="username"
               onChange={this.handleChange}
-              className="rounded-input" // Aplica la clase CSS aquí
+              className="rounded-input" // Apply the CSS class here
             />
             <input
               placeholder="Contraseña"
               type="password"
-              className="form-control rounded-input" // Aplica la clase CSS aquí
+              className="form-control rounded-input" // Apply the CSS class here
               name="password"
               onChange={this.handleChange}
             />
